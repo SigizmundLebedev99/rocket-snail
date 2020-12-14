271bb85f6241f7c1f21408c8fb960952f122185d
dmlebedev <dmlebedev@briogroup.ru>
2020-12-14 11:42:21 +0300

    ~

--- src/general-components/DragDropCom.ts
+++ src/general-components/DragDropCom.ts
@@ -1,30 +1,30 @@
 import { Component, IState } from '../core/Component';
 import { Vector } from '../primitives/Vector';
 
-export class DragDrop extends Component{
-    mouseDown = false;
+export class DragDrop extends Component {
+  mouseDown = false;
 
-    OnUpdate(state: IState): void {
-        let {mouseState, node} = state;
-        if(mouseState.IsCaptured && ! this.mouseDown)
-            this.mouseDown = true;
-        if(!mouseState.IsCaptured && this.mouseDown)
-            this.mouseDown = false;
-        if(!this.mouseDown)
-            return;
-        
-        let d = mouseState.Movement;
+  OnUpdate(state: IState): void {
+    let { mouseState, node } = state;
+    if (mouseState.IsCaptured && !this.mouseDown)
+      this.mouseDown = true;
+    if (!mouseState.IsCaptured && this.mouseDown)
+      this.mouseDown = false;
+    if (!this.mouseDown)
+      return;
 
-        if(d == null)
-            return;
+    let d = mouseState.Movement;
 
-        if(node.ApplyTransform && node.Parent != null){
-            let scale = node.Parent.TotalScale;
-            var v = new Vector(d.x / scale.x, d.y / scale.y);
-            v.Rotate(-node.TotalRotation);
-            node.Transition.AddV(v);
-        }
-        else
-            node.Transition.Add(d.x, d.y);
+    if (d == null)
+      return;
+
+    if (node.ApplyTransform && node.Parent != null) {
+      let scale = node.Parent.TotalScale;
+      var v = new Vector(d.x / scale.x, d.y / scale.y);
+      v.Rotate(-node.TotalRotation);
+      node.Transition.AddV(v);
     }
+    else
+      node.Transition.Add(d.x, d.y);
+  }
 }
